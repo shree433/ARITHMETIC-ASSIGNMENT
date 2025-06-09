73605223e1d401e993255e43aec188f27f8e4b0a
shree433 <shreemudshi0@gmail.com>
2025-06-10 00:48:44 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,10 @@
+#performing arithmetic oprations
+
+a=int(input("Entre the first number:" ))
+b=int(input("Entre the second number:" ))
+
+print("Addition:",a+b)
+print("substraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+
---
+++ greet.py
@@ -0,0 +1,4 @@
+a=input("Entre your first name:")
+b=input("Entre your last name:")
+
+print("hello",a+b ,"!" ,"welcome to the python program")
